f9c4b8b316d64a0bda499dc47de574077c611a9e
Vivian Berger <vivianberger028@gmail.com>
2022-11-07 10:00:43 +0100

    Implemented testing on tokenize function.

--- src/main/scala/compilersandbox/Tokenizer.scala
+++ src/main/scala/compilersandbox/Tokenizer.scala
@@ -77,35 +77,3 @@ case class Parenthesis(value: ParenthesisKind) extends Token
 enum ParenthesisKind {
   case Open, Close
 }
-
-@main
-def tokenizerMain(): Unit = {
-
-  val input1 = "59+3"
-  val tokens1 = Tokenizer.tokenize(input1, Start, List.empty)
-  println(s"$input1: $tokens1")
-
-  val input2 = "(5+3)"
-  val tokens2 = Tokenizer.tokenize(input2, Start, List.empty)
-  println(s"$input2: $tokens2")
-
-  val input3 = "5*(8-1)"
-  val tokens3 = Tokenizer.tokenize(input3, Start, List.empty)
-  println(s"$input3: $tokens3")
-
-  val input4 = "(-8+1)"
-  val tokens4 = Tokenizer.tokenize(input4, Start, List.empty)
-  println(s"$input4: $tokens4")
-
-  val input5 = "-8+1"
-  val tokens5 = Tokenizer.tokenize(input5, Start, List.empty)
-  println(s"$input5: $tokens5")
-
-  val input6 = "-84+12"
-  val tokens6 = Tokenizer.tokenize(input6, Start, List.empty)
-  println(s"$input6: $tokens6")
-
-  val input7 = "(100-84)+12"
-  val tokens7 = Tokenizer.tokenize(input7, Start, List.empty)
-  println(s"$input7: $tokens7")
-}
---
+++ src/test/scala/compilersandbox/TokenizerSpec.scala
@@ -0,0 +1,60 @@
+package compilersandbox
+
+import compilersandbox.ParenthesisKind.{Close, Open}
+import org.scalatest.freespec.AnyFreeSpec
+
+class TokenizerSpec extends AnyFreeSpec {
+
+  "A Tokenizer" - {
+
+    "should tokenize 59+3" in {
+
+      val expected = List(Start, Number("59"), Operator("+"), Number("3"), End)
+      val result = Tokenizer.tokenize("59+3", Start, List.empty)
+      assert(result == expected)
+    }
+
+    "should tokenize (5+3)" in {
+
+      val expected = List(Start, Parenthesis(Open), Number("5"), Operator("+"), Number("3"), Parenthesis(Close), End)
+      val result = Tokenizer.tokenize("(5+3)", Start, List.empty)
+      assert(result == expected)
+    }
+
+    "should tokenize 5*(8-1)" in {
+
+      val expected = List(Start, Number("5"), Operator("*"), Parenthesis(Open), Number("8"), Operator("-"), Number("1"), Parenthesis(Close), End)
+      val result = Tokenizer.tokenize("5*(8-1)", Start, List.empty)
+      assert(result == expected)
+
+    }
+
+    "should tokenize (-8+1)" in {
+
+      val expected = List(Start, Parenthesis(Open), Number("-8"), Operator("+"), Number("1"), Parenthesis(Close), End)
+      val result = Tokenizer.tokenize("(-8+1)", Start, List.empty)
+      assert(result == expected)
+    }
+
+    "should tokenize -8+1" in {
+
+      val expected = List(Start, Number("-8"), Operator("+"), Number("1"), End)
+      val result = Tokenizer.tokenize("-8+1", Start, List.empty)
+      assert(result == expected)
+    }
+
+    "should tokenize -84+12" in {
+
+      val expected = List(Start, Number("-84"), Operator("+"), Number("12"), End)
+      val result = Tokenizer.tokenize("-84+12", Start, List.empty)
+      assert(result == expected)
+    }
+
+    "should tokenize (100-84)+12" in {
+
+      val expected = List(Start, Parenthesis(Open), Number("100"), Operator("-"), Number("84"), Parenthesis(Close), Operator("+"), Number("12"), End)
+      val result = Tokenizer.tokenize("(100-84)+12", Start, List.empty)
+      assert(result == expected)
+    }
+  }
+}
